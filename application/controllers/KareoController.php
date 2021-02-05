f8d88f53006e60b686a79f345b87babc0282cea8
Jesus Omar <jocg210@gmail.com>
2021-02-05 10:33:08 -0600

    all codes

--- application/controllers/KareoController.php
+++ application/controllers/KareoController.php
@@ -32,58 +32,34 @@ class KareoController extends CI_Controller {
     {
         $this->load->helper('url');
 
-		$this->load->library('session');
-		//$this->session->set_userdata('sesion', 1);
+
+        $AppointmentStatus = $this->input->post("AppointmentStatus");
+        $AppointmentType = $this->input->post("AppointmentType");
+        $EndTime = $this->input->post("EndTime");
+        $ForRecare = $this->input->post("ForRecare");
+        $IsDeleted = $this->input->post("IsDeleted");
+        $IsGroupAppointment = $this->input->post("IsGroupAppointment");
+        $IsRecurring = $this->input->post("IsRecurring");
+        $DateOfBirth = $this->input->post("DateOfBirth");
+        $FirstName = $this->input->post("FirstName");
+        $GenderId = $this->input->post("GenderId");
+        $HomePhone = $this->input->post("HomePhone");
+        $LastName  = $this->input->post("LastName ");
+        $MobilePhone = $this->input->post("MobilePhone");
+        $PatientId = $this->input->post("PatientId");
+        $PracticeId = $this->input->post("PracticeId");
+        $ProviderId = $this->input->post("ProviderId");
+        $ServiceLocationId = $this->input->post("ServiceLocationId");
+        $StartTime  = $this->input->post("StartTime ");
+        $WasCreatedOnline = $this->input->post("WasCreatedOnline");
 		
 
-		//$this->load->database('default');
-		//$this->load->model('strabajador');
-			
-        $this->request = json_decode(file_get_contents('php://input'));
-        
-        $curl = curl_init();
-
-curl_setopt($curl , CURLOPT_URL , 'https://www.zohoapis.com/crm/v2/functions/serverlesstest/actions/execute?auth_type=apikey&zapikey=1003.bd8bfa9be391059bc021faf645da2de5.35e6095fb73458fc45f37cba022b1791');
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-$ress = curl_exec($curl);
-
-$jsonn = json_decode($ress);
-$details = $jsonn->details;
-$output = $details->output;
-
-echo $ress . '<br/><br/>';
-
-$dataMap = json_decode($output);
-
-print_r($output);
-
-echo '<br/><br/>';
-
-try{
+        try{
     $user = 'antonio@marksgroup.net';
     $password = 'PLKu4Wf*';
     $customerKey = 'g74nf36tq59z';
     //----------------------------------------
-    $AppointmentStatus = '';
-    $AppointmentType = '';
-    $EndTime = $dataMap->EndTime;
-    $ForRecare = $dataMap->ForRecare;
-    $IsDeleted = $dataMap->IsDeleted;
-    $IsGroupAppointment = $dataMap->IsGroupAppointment;
-    $IsRecurring = $dataMap->IsRecurring;
-    $DateOfBirth = $dataMap->DateOfBirth;
-    $FirstName = $dataMap->FirstName;
-    $GenderId = $dataMap->GenderId;
-    $HomePhone = $dataMap->HomePhone;
-    $LastName  = $dataMap->LastName ;
-    $MobilePhone = $dataMap->MobilePhone;
-    $PatientId = $dataMap->PatientId;
-    $PracticeId = $dataMap->PracticeId;
-    $ProviderId = $dataMap->ProviderId;
-    $ServiceLocationId = $dataMap->ServiceLocationId;
-    $StartTime  = $dataMap->StartTime ;
-    $WasCreatedOnline = $dataMap->WasCreatedOnline;
+    
     // $AppointmentStatus = 'Scheduled';
     // $AppointmentType = 'P';
     // $EndTime = '2021-01-28T17:30:00.000Z';
@@ -241,25 +217,156 @@ public function CreatePatient()
     
     /*$curl = curl_init();
 
-curl_setopt($curl , CURLOPT_URL , 'https://www.zohoapis.com/crm/v2/functions/createpatients/actions/execute?auth_type=apikey&zapikey=1003.bd8bfa9be391059bc021faf645da2de5.35e6095fb73458fc45f37cba022b1791');
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl , CURLOPT_URL , 'https://www.zohoapis.com/crm/v2/functions/createpatients/actions/execute?auth_type=apikey&zapikey=1003.bd8bfa9be391059bc021faf645da2de5.35e6095fb73458fc45f37cba022b1791');
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-$ress = curl_exec($curl);
+    $ress = curl_exec($curl);
 
-echo $ress;
-$jsonn=json_decode($ress);
-$details=$this->input->post("tails;
-$output=$details->output;
+    echo $ress;
+    $jsonn=json_decode($ress);
+    $details=$this->input->post("tails;
+    $output=$details->output;
 
-echo $ress . '<br/><br/>';
+    echo $ress . '<br/><br/>';
 
-$dataMap=json_decode($jsonn);
+    $dataMap=json_decode($jsonn);
 
-print_r($output. '<br/><br/>');*/
-//return $AddressLine1;
+    print_r($output. '<br/><br/>');*/
+    //return $AddressLine1;
+}
+public function UpdatePatient()
+{
+
+    $this->load->helper('url');
+
+    $AddressLine1 = $this->input->post("AddressLine1");
+    $AddressLine2 = $this->input->post("AddressLine2");
+    $City = $this->input->post("City");
+    $Country = $this->input->post("Country");
+    $DateofBirth = $this->input->post("DateofBirth");
+    $EmailAddress = $this->input->post("EmailAddress");
+    $FirstName = $this->input->post("FirstName");
+    $Gender = $this->input->post("Gender");
+    $HomePhone = $this->input->post("HomePhone");
+    $LastName = $this->input->post("LastName");
+    $MobilePhone = $this->input->post("MobilePhone");
+    $PatientID = $this->input->post("PatientID");
+    $State = $this->input->post("State");
+    $WorkPhone = $this->input->post("WorkPhone");
+    $ZipCode = $this->input->post("ZipCode");
+    $PracticeID = $this->input->post("PracticeID");
+    $PracticeName = $this->input->post("PracticeName");
+    
+    try{
+        $user = 'antonio@marksgroup.net';
+        $password = 'PLKu4Wf*';
+        $customerKey = 'g74nf36tq59z';
+        //-------------------------------------
+
+
+        $wsdl = 'https://webservice.kareo.com/services/soap/2.1/KareoServices.svc?singleWsdl';
+        $client = new SoapClient($wsdl);
+
+        // 'Authorizations'=>[
+        //     'InsurancePolicyAuthorizationCreateReq'=>[
+        //         'ContactFullName'=>$ContactFullName, 'ContactPhone'=>$ContactPhone,'InsurancePlanName'=>$InsurancePlanName,'Number'=>$Number,'NumberOfVisits'=>$NumberOfVisits ]],
+        // $Cases=array(
+        //     'PatientCaseCreateReq'=>[
+        //     'Policies'=>[
+        //         'InsurancePolicyCreateReq'=>[
+        //             'Copay'=>$Copay,'Deductible'=>$Deductible, 
+        //             'Insured'=>[
+        //                 'DateofBirth'=>$InsuredDateofBirth, 'FirstName'=>$InsuredFirstName, 'LastName'=>$InsuredLastName, 'MiddleName'=>$InsuredMiddleName], 
+        //             'PlanName'=>$PlanName, 'PolicyGroupNumber'=>$PolicyGroupNumber, 'PolicyNotes'=>$PolicyNotes, 'PolicyNumber'=>$PolicyNumber]]]
+        // );
+        $Practice=array(
+            'PracticeID'=>$PracticeID, 'PracticeName'=>$PracticeName
+        );
+
+
+        $UpdatePatientReq = array (
+            'RequestHeader' => [
+                'User' => $user, 'Password' => $password, 'CustomerKey' => $customerKey],
+            'Patient'=>[
+                'AddressLine1'=>$AddressLine1, 'AddressLine2'=>$AddressLine2, 'City'=>$City, 'Country'=>$Country, 'DateofBirth'=>$DateofBirth, 'EmailAddress'=>$EmailAddress, 'FirstName'=>$FirstName, 'Gender'=>$Gender, 'HomePhone'=>$HomePhone, 'LastName'=>$LastName, 'MobilePhone'=>$MobilePhone,'PatientID'=>$PatientID, 'Practice'=>$Practice, 'State'=>$State, 'WorkPhone'=>$WorkPhone, 'ZipCode'=>$ZipCode]
+        );
+        // echo json_encode($UpdatePatientReq);
+
+        $params = array('UpdatePatientReq' => $UpdatePatientReq);
+        $response = $client->UpdatePatient($params)->UpdatePatientResult;
+
+        print_r ($response);
+
+        // foreach($response->Patients->PatientData as &$value)
+        // {
+        //     print($value->PatientFullName. '<br />');
+        // }
+    } catch (Exception $err) {
+        print "Error: ". $err->getMessage();
+    }
+
 }
 
+public function UpdateAppointment()
+{
 
+    $this->load->helper('url');
+
+    $AppointmentId=$this->input->post("AppointmentId");
+    $AppointmentReasonId=$this->input->post("AppointmentReasonId"); 
+    $AppointmentStatus =$this->input->post("AppointmentStatus");
+    $EndTime =$this->input->post("EndTime");
+    $IsGroupAppointment =$this->input->post("IsGroupAppointment");
+    $IsRecurring =$this->input->post("IsRecurring");
+    $MaxAttendees=$this->input->post("MaxAttendees");
+    $PatientId =$this->input->post("PatientId");
+    //$PracticeId =$this->input->post("PracticeId");
+    $ProviderId =$this->input->post("ProviderId");
+    $ResourceId=$this->input->post("ResourceId");
+    $UpdatedBy=$this->input->post("UpdatedBy");
+    $ServiceLocationId =$this->input->post("ServiceLocationId");
+    $StartTime =$this->input->post("StartTime");
+
+    try{
+        $user = 'antonio@marksgroup.net';
+        $password = 'PLKu4Wf*';
+        $customerKey = 'g74nf36tq59z';
+    
+    
+       
+        
+        
+        
+        //$PatientSummary = ['DateOfBirth' => $DateOfBirth, 'FirstName' => $FirstName, 'GenderId' => $GenderId, 'HomePhone' => $HomePhone, 'LastName' => $LastName, 'MobilePhone' => $MobilePhone, 'PatientId' => $PatientId];
+        
+        $wsdl = 'https://webservice.kareo.com/services/soap/2.1/KareoServices.svc?singleWsdl';
+        $client = new SoapClient($wsdl);
+        
+        $request = array (
+            'RequestHeader' => ['User' => $user, 'Password' => $password, 'CustomerKey' => $customerKey],
+            'Appointment' => ['AppointmentId'=>$AppointmentId, 'AppointmentReasonId'=>$AppointmentReasonId,'AppointmentStatus' => $AppointmentStatus, 'EndTime' => $EndTime,'IsGroupAppointment' => $IsGroupAppointment, 'IsRecurring' => $IsRecurring, 'PatientId'=>$PatientId, 'ProviderId' => $ProviderId, 'ResourceId'=>$ResourceId,'ServiceLocationId' => $ServiceLocationId, 'StartTime' => $StartTime, 'UpdatedBy'=>$UpdatedBy],
+        );
+        // echo json_encode($request) . '<br />';
+    
+    
+        $params = array('request' => $request);
+        $response = $client->UpdateAppointment($params)->UpdateAppointmentResult;
+    
+        print_r ($response);
+    
+        echo '<br />';
+        $UpAppointmentData =json_encode($response);
+    
+        // foreach($response->Patients->PatientData as &$value)
+        // {
+        //     print($value->PatientFullName. '<br />');
+        // }
+    } catch (Exception $err) {
+        print "Error: ". $err->getMessage();
+    }
+
+
+}
 
 
 
@@ -277,23 +384,23 @@ print_r($output. '<br/><br/>');*/
         $this->request = json_decode(file_get_contents('php://input'));
         
         $AddressLine1="Test line 1";
-$AddressLine2="Test line 2";
-$City="Philadelphia";
-$Country="U.S.";
-$DateofBirth="1970-01-01";
-$EmailAddress="TestQA2@gmail.com";
-$FirstName="TEST TEST";
-$Gender="Male";
-$HomePhone="(871)785-8965";
-$LastName="Omar";
-$MobilePhone="(871)654-8569";
-$PatientID=26;
-$State="PA";
-$WorkPhone="(871)854-2154";
-$ZipCode=19148;
-$PracticeID=3;
-$PracticeName="Sandbox";
-$user = 'antonio@marksgroup.net';
+        $AddressLine2="Test line 2";
+        $City="Philadelphia";
+        $Country="U.S.";
+        $DateofBirth="1970-01-01";
+        $EmailAddress="TestQA2@gmail.com";
+        $FirstName="TEST TEST";
+        $Gender="Male";
+        $HomePhone="(871)785-8965";
+        $LastName="Omar";
+        $MobilePhone="(871)654-8569";
+        $PatientID=26;
+        $State="PA";
+        $WorkPhone="(871)854-2154";
+        $ZipCode=19148;
+        $PracticeID=3;
+        $PracticeName="Sandbox";
+        $user = 'antonio@marksgroup.net';
     $password = 'PLKu4Wf*';
     $customerKey = 'g74nf36tq59z';
     $Practice=array(
